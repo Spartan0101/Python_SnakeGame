d8d0ed996e31cd29d875bd092f70fa8ab03d28d3
Spartan1010 <tranulovianos@gmail.com>
2024-08-09 11:38:52 +0100

    Snake Game v2

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ screen.title("My Snake Game!")
 screen.tracer(0)
 
 snake = Snake()
-# screen.listen()
-# screen.onkey(snake.up, "Up")
-# screen.onkey(snake.down, "Down")
-# screen.onkey(snake.left, "Left")
-# screen.onkey(snake.right, "Right")
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 games_is_on = True
 while games_is_on:
